09a668e8a41ddd1d6286d034ed23c661c8beec2c
conatus-primus <brosovoe@mail.ru>
2019-08-18 19:57:51 +0300

    Codec

--- Algo/4/test.bat
+++ Algo/4/test.bat
@@ -1,10 +1,10 @@
-set path_exe=D:\Projects\otus\Algo\4\Release
+п»їset path_exe=D:\Projects\otus\Algo\4\Release
 set path_test="D:\Projects\otus\Algo\4\2019-08-12_Chess-Tasks"
 
 %path_exe%/4.exe 1743 "%path_test%/1743.1.FEN - ASCII"
 %path_exe%/4.exe 1744 "%path_test%/1744.1.FEN - BITS _"
-%path_exe%/4.exe 1745 "%path_test%/1745.1.Сборка и разборка"
-%path_exe%/4.exe 1746 "%path_test%/1746.1.Счётчик ходов"
+%path_exe%/4.exe 1745 "%path_test%/1745.1.вЂ”Р±РѕСЂРєР° Рё СЂР°Р·Р±РѕСЂРєР°"
+%path_exe%/4.exe 1746 "%path_test%/1746.1.вЂ”С‡Р„С‚С‡РёРє С…РѕРґРѕРІ"
 
 
 pause
